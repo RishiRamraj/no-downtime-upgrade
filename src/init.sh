5e493a6d14f57850a63427cb3d377d846d40cc0a
Rishi Ramraj <thereisnocowlevel@gmail.com>
2021-08-20 05:05:32 -0400

    Remove unique constraints

--- src/init.sh
+++ src/init.sh
@@ -2,12 +2,12 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<- EOSQL
 CREATE TABLE users(
   id    SERIAL PRIMARY KEY,
-  email VARCHAR(40) NOT NULL UNIQUE
+  email VARCHAR(40) NOT NULL
 );
 CREATE TABLE posts(
   id      SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(id),
-  title   VARCHAR(100) NOT NULL UNIQUE
+  title   VARCHAR(100) NOT NULL
 );
 CREATE TABLE comments(
   id      SERIAL PRIMARY KEY,
