ca448a2c3ae5e73bcffe91eadd44e7ba5aa9edec
Rishi Ramraj <thereisnocowlevel@gmail.com>
2021-08-20 03:48:14 -0400

    Add src database

---
+++ src/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<- EOSQL
+CREATE TABLE users(
+  id    SERIAL PRIMARY KEY,
+  email VARCHAR(40) NOT NULL UNIQUE
+);
+CREATE TABLE posts(
+  id      SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  title   VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE comments(
+  id      SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  post_id INTEGER NOT NULL REFERENCES posts(id),
+  body    VARCHAR(500) NOT NULL
+);
+EOSQL
